84ce945e41c5aabee86e8fe2c067fa30a4d36306
sanket008 <sanket.p.thombare@gmail.com>
2022-02-27 22:49:32 +0530

    nodemon

--- src/index.js
+++ src/index.js
@@ -154,7 +154,7 @@ app.post("/paynow", [parseUrl, parseJson], (req, res) => {
     params["ORDER_ID"] = "TEST_" + new Date().getTime();
     params["CUST_ID"] = paymentDetails.customerId;
     params["TXN_AMOUNT"] = paymentDetails.amount;
-    params["CALLBACK_URL"] = "http://localhost:5000/review";
+    params["CALLBACK_URL"] = "https://theory-clone-backend.herokuapp.com/review";
     params["EMAIL"] = paymentDetails.customerEmail;
     params["MOBILE_NO"] = paymentDetails.customerPhone;
 
